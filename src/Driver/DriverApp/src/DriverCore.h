7bc9d407a742ce1b6d10d4b1a196ff4b30090267
Rida <rida.pgm@gmail.com>
2021-03-04 18:22:03 +0100

    DriverCore update
    (Partial) Auto COMport détection

--- src/Driver/DriverApp/src/DriverCore.h
+++ src/Driver/DriverApp/src/DriverCore.h
@@ -1,13 +1,25 @@
 #pragma once
+#include <Windows.h>
+#include <vector>
+#include <string>
 
 class SerialPort
 {
 private:
-	bool connected = 0;
+	HANDLE _streamHandle;
+	bool _connState;
+	LPCSTR _portName;
+	COMSTAT _status;
+	DWORD _errors;
 public:
-	SerialPort();
+	SerialPort(const char* portName);
+	~SerialPort();
+
+	virtual void _CloseConn();
 	virtual void HelloWorld();
 	virtual bool isConnected();
+	virtual std::vector<std::string> _SerialList();
+	virtual std::string _w_to_s(std::wstring WSTRING);
+	virtual std::string _autoSelectPort(std::vector<std::string> serialList);
 	virtual int readSerialPort(char* buffer, unsigned int buf_size);
-	virtual bool writeSerialPort(char* buffer, unsigned int buf_size);
 };
