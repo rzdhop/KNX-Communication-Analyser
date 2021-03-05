7865ffc33b082313e1caf5d3ec89e6ba435123bb
Rida <rida.pgm@gmail.com>
2021-03-05 19:25:15 +0100

    Optimisation of Debugging
    Adding GetLastError()

--- src/Driver/DriverApp/src/DriverApp.cpp
+++ src/Driver/DriverApp/src/DriverApp.cpp
@@ -1,16 +1,15 @@
+#define WIN32_LEAN_AND_MEAN
+
 #include <Windows.h>
 #include <iostream>
 #include "../../DriverCore/DriverCore/DriverCore.h"
 
 int main()
 {
-	const unsigned int buffer_size(10);
-	const char* Buffer[buffer_size] = { 0 };
-
 	//load dllinto addr space of client proc
-	HMODULE hDLL = LoadLibraryExW(L"DriverCore.dll", nullptr, 0);
+	HMODULE hDLL = LoadLibraryExW(L"../Debug/DriverCore.dll", nullptr, 0);
 	if (!hDLL) {
-		std::cout << "Cannot load DLL !";
+		std::cout << "Cannot load DLL ! " << GetLastError();
 		return ERROR;
 	}
 
@@ -18,15 +17,13 @@ int main()
 	using GreetingType = SerialPort * (__cdecl*)(); //type def ptr to func w/ retType<void> argv/c<void>
 	GreetingType pSerialPort = reinterpret_cast<GreetingType>(GetProcAddress(hDLL, "CreateSerialPort"));
 	if (!pSerialPort) {
-		std::cout << "Cannot load Function !";
+		std::cout << "Cannot load Function ! " << GetLastError();
 		return ERROR;
 	}
 	SerialPort* CSerialPort = pSerialPort();
 
 	CSerialPort->HelloWorld();
 	std::cout << "\n\n";
-	
-	CSerialPort->_SerialList();
 
 	return 0;
 }
